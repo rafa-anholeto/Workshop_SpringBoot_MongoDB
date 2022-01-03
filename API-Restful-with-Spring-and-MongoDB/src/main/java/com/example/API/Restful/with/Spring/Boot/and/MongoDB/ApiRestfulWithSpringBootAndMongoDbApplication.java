866a1dab2404935adefaf64a6160be57714b82b4
rafa-anholeto <rafaanholeto3@gmail.com>
2022-01-03 16:10:56 -0300

    Primeiro commit projeto API Restful with MongoDB

---
+++ API-Restful-with-Spring-and-MongoDB/src/main/java/com/example/API/Restful/with/Spring/Boot/and/MongoDB/ApiRestfulWithSpringBootAndMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.API.Restful.with.Spring.Boot.and.MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulWithSpringBootAndMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulWithSpringBootAndMongoDbApplication.class, args);
+	}
+
+}
---
+++ API-Restful-with-Spring-and-MongoDB/src/test/java/com/example/API/Restful/with/Spring/Boot/and/MongoDB/ApiRestfulWithSpringBootAndMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API.Restful.with.Spring.Boot.and.MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulWithSpringBootAndMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
