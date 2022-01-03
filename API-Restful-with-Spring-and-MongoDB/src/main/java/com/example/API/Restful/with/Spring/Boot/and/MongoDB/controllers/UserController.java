43d79cc22dacf9df106fe4d5ad86f4afc1841d27
rafa-anholeto <rafaanholeto3@gmail.com>
2022-01-03 16:39:52 -0300

    Entity user e REST funcionando

---
+++ API-Restful-with-Spring-and-MongoDB/src/main/java/com/example/API/Restful/with/Spring/Boot/and/MongoDB/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.example.API.Restful.with.Spring.Boot.and.MongoDB.controllers;
+
+import com.example.API.Restful.with.Spring.Boot.and.MongoDB.domain.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        User maria = new User("1", "Maria Brown", "maria@gmail.com");
+        User alex = new User("2", "Alex Green", "alex@gmail.com");
+        List<User> list = new ArrayList<>();
+        list.addAll(Arrays.asList(maria,alex));
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+}
