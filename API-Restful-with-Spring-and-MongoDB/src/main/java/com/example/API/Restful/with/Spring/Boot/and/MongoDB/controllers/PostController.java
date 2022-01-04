d94f9ae933713d3fbdfc50a512703a7c184bf145
rafa-anholeto <rafaanholeto3@gmail.com>
2022-01-04 13:24:25 -0300

    Consulta com vários critérios

--- API-Restful-with-Spring-and-MongoDB/src/main/java/com/example/API/Restful/with/Spring/Boot/and/MongoDB/controllers/PostController.java
+++ API-Restful-with-Spring-and-MongoDB/src/main/java/com/example/API/Restful/with/Spring/Boot/and/MongoDB/controllers/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -27,6 +28,18 @@ public class PostController {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
         text = URL.decodeParam(text);
         List<Post> list = postService.findByTitle(text);
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+                                                 @RequestParam(value = "text", defaultValue = "") String minDate,
+                                                 @RequestParam(value = "text", defaultValue = "") String maxDate){
+
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text, min, max);
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
