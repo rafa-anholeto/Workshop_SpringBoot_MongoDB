409b87a84ae3fa3abadc5147d38fc9487be96b6b
rafa-anholeto <rafaanholeto3@gmail.com>
2022-01-04 11:04:18 -0300

    Projeção dos dados do autor com DTO

--- API-Restful-with-Spring-and-MongoDB/src/main/java/com/example/API/Restful/with/Spring/Boot/and/MongoDB/config/Instantiation.java
+++ API-Restful-with-Spring-and-MongoDB/src/main/java/com/example/API/Restful/with/Spring/Boot/and/MongoDB/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.example.API.Restful.with.Spring.Boot.and.MongoDB.config;
 
 import com.example.API.Restful.with.Spring.Boot.and.MongoDB.domain.Post;
 import com.example.API.Restful.with.Spring.Boot.and.MongoDB.domain.User;
+import com.example.API.Restful.with.Spring.Boot.and.MongoDB.dto.AuthorDTO;
 import com.example.API.Restful.with.Spring.Boot.and.MongoDB.repositories.PostRepository;
 import com.example.API.Restful.with.Spring.Boot.and.MongoDB.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,12 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
